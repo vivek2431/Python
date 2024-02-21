fe3b8621f3e2316463173438f3ece8c7edfe759b
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-02-21 22:52:40 +0530

    string-split.py

--- string-split.py
+++ string-split.py
@@ -7,3 +7,10 @@ print("Words:", word)
 text = "   Some spaces around   "
 stripped_text = text.strip()
 print("Stripped text:", stripped_text)
+
+# Substring
+text = "python is dynamically typed language"
+substring="is"
+if substring in text:
+    print(substring, "found in the text")
+  
