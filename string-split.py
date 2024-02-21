50393fb44823167d8cf24983419d8b20cb4be8a3
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-02-21 22:45:11 +0530

    string-split.py

--- string-split.py
+++ string-split.py
@@ -1,3 +1,9 @@
 text="python is awesome"
 word= text.split()
 print("Words:", word)
+
+# string Sprit
+
+text = "   Some spaces around   "
+stripped_text = text.strip()
+print("Stripped text:", stripped_text)
