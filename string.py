46b71bc0b4cb3d7f7df90ef67881ac740158c30c
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-03-14 13:19:10 +0530

    string.py

--- string.py
+++ string.py
@@ -8,3 +8,10 @@ str1 = "Hello"
 str2 = "World"
 result = str1 + " " + str2
 print(result)
+
+# Ways to print code of strings
+
+print('String manipulation Exercise')
+print('String Concatination is done with "+" Sign')
+print('e.g. print("Hello "+"Vivek)')
+print('New lines can be created with a backslash and n')
