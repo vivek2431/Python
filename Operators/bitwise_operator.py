26e359df7ae31cd8a5221b106f05e28116ffc3b5
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-03-22 13:07:06 +0530

    bitwise_operator.py

---
+++ Operators/bitwise_operator.py
@@ -0,0 +1,33 @@
+### Bitwise and(&) operator
+
+a=5
+b=4
+print(a & b)
+
+### Bitwise or(|) operator
+
+a=5
+b=4
+print(a | b)
+
+### Bitwise XOR(^) operator
+
+a=5
+b=4
+print(a ^ b)
+
+### Bitise Not(~) operator
+
+a=5
+b=4
+print (~a)
+
+### Bitwise left shift(<<) operator
+
+a=5
+print(a << 2)
+
+### Bitwise right shift(>>) operator
+
+a=5
+print(a >> 2)
