e22025563a558c9f54488bedf11bb03bc5cc800d
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-03-25 14:20:35 +0530

    f_strings.py

---
+++ f_strings.py
@@ -0,0 +1,8 @@
+name='kabir'
+age= 16
+height= 1.5
+
+print(f"My name is {name}. I am {age} years old. My height is {height} meters.")
+print(f"My name is {name}. I am {age*2} years old. My height is {height} meters.")
+# Output:My name is kabir. I am 16 years old. My height is 1.5 meters.
+# Output:My name is kabir. I am 32 years old. My height is 1.5 meters.
