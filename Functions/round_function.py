5b9ddc80bd19cb01bee45276cf3905318ba63adf
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-03-25 13:44:47 +0530

    round_function.py

---
+++ Functions/round_function.py
@@ -0,0 +1,19 @@
+print(round(7,2))
+print(type(round(7,2))) #output: 7 and 'int'
+
+print(round(7.65526,2))
+print(type(round(7.65526,2))) #output: 7.66 and 'float'
+
+print(round(7.65526))
+print(type(round(7.65526))) #output: 8 and 'int'
+
+print(round(7.5))
+print(type(round(7.5))) #output: 8 and 'int' Nearest even value
+
+print(round(6.5))
+print(type(round(6.5))) #output: 6 and 'int' Nearest even value
+
+print(round(-8/3,2))
+print(type(round(-8/3,2))) #output: -2.67
+
+print(type(round(675,-1)))
