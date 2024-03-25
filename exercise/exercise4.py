2f86d1085f8b886688857316a084de7b7f291049
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-03-25 14:31:18 +0530

    exercise4.py

---
+++ exercise/exercise4.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age"))
+
+years_left=90-age
+days_left=years_left*365
+months_left=years_left*12
+weeks_left=years_left*52
+
+print(f"You have {days_left} days, {weeks_left} weeks and {months_left} months left")
+
+# Output: Enter your age27
+# You have 22995 days, 3276 weeks and 756 months left
