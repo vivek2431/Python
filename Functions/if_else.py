dbfb9ec41528e6783ea212449a873c690c3dc9ec
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-04 14:19:41 +0530

    if_else.py

---
+++ Functions/if_else.py
@@ -0,0 +1,11 @@
+height=int(input("enter height in feet:"))
+
+if(height>3):
+    print("Buy Token")
+else:
+    print("No Token Required")
+
+#output : Enter height: 5
+# Buy Token
+# Enter height: 2
+# "No Token Required"
