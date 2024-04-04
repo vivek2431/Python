f75b2883f20aa8280a35a79d65a33a35e8afa07e
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-04 14:50:48 +0530

    exercise5.py

---
+++ exercise/exercise5.py
@@ -0,0 +1,11 @@
+# find whether given number is even or odd
+
+n=int(input("enter the number:"))
+
+if n%2==0:
+    print("N is an Even number")
+else:
+    print("N is an Odd number")
+
+#output: 72, 72 is an even number
+#output: 51, 51 is an odd number
