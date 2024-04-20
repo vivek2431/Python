5588c168cc03a1859f721171504936f682eda91a
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-20 12:26:08 +0530

    random_module.py

---
+++ Data /random_module.py
@@ -0,0 +1,12 @@
+# to use this code first uncomment(remove #) from the code then run it in your IDE
+import random
+
+#a=random.randint(1,8)
+#a=random.randrange(1,7)
+#a=random.random()
+#a=random.uniform(1,3)
+l=[2,5,85,46,72,-5]
+#a=random.choice(l)
+random.shuffle(l)
+#print(l)
+print(a)
