a494220ee42974f7f07044ffada84976a7a9c6b6
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-22 11:47:41 +0530

    nested_list.py

---
+++ Functions/nested_list.py
@@ -0,0 +1,24 @@
+list1=[65,98,31,63,57,[-9,51,23],93,81]
+print(len(list1))
+print(list1[5])
+print(list1[5][1])
+print(list1[5][-3])
+print(list1[len(list1)-3])
+print(list1[5][0:2])
+
+list2=[10,34,90,['Mohan','Shyam','Ram'],89]
+print(len(list2))
+print(list2[3][2])
+print(list2[len(list2)-2])
+print(list2[3][0:3])
+
+# Output : 8
+[-9, 51, 23]
+51
+-9
+[-9, 51, 23]
+[-9, 51]
+5
+Ram
+['Mohan', 'Shyam', 'Ram']
+['Mohan', 'Shyam', 'Ram']
