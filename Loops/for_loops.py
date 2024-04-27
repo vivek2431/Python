1b238fc9612e580ffa9a1cc8257817eaa8ab44bc
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-27 22:02:08 +0530

    for_loops.py

---
+++ Loops/for_loops.py
@@ -0,0 +1,19 @@
+name=["Vivek","Shyam","Mohan"]
+for p in name:
+    print(p)
+    if p == "Shyam":
+        print("Hey, 'It's me")
+
+numbers=[4,5,-9,7,3]
+squares=[]
+for i in numbers:
+    square = i ** 2
+    squares.append(square)
+print("The list of squares is:",squares)
+
+# Output : 
+Vivek
+Shyam
+Hey, 'It's me
+Mohan
+The list of squares is: [16, 25, 81, 49, 9]
