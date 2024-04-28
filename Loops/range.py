42b4b26d7516c5df916355846ef5d29857dd2b4a
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-28 11:54:42 +0530

    range.py

---
+++ Loops/range.py
@@ -0,0 +1,25 @@
+a=range(2,10,2)
+for i in a:
+    print(i)
+b=range(7,0,-1)
+for i in b:
+    print(i)
+
+total=0
+for i in range(1,101):
+    total += i
+print(total)
+
+# Output : 
+2
+4
+6
+8
+7
+6
+5
+4
+3
+2
+1
+5050
