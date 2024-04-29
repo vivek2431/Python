48bde4a9d75fdbf30d15ebcee7f5951892ef2bc4
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-29 13:34:12 +0530

    while_loop.py

---
+++ Loops/while_loop.py
@@ -0,0 +1,25 @@
+count=1
+while count<=5:
+    print(count)
+    count += 1
+print("out of the loop")
+
+count2=5
+while count2>0:
+    print(count2)
+    count2 -= 1
+print("Out from loop")
+
+# Output :
+1
+2
+3
+4
+5
+out of the loop
+5
+4
+3
+2
+1
+Out from loop
