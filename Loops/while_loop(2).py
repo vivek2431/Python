bcb384bb363edb0469ccab272dbbf0817b03ab78
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-29 13:35:35 +0530

    while_loop(2).py

---
+++ Loops/while_loop(2).py
@@ -0,0 +1,18 @@
+count = 1
+while count <=10:
+    print(count)
+    count += 1
+    if count == 7:
+        break
+else:
+ print("in else block")
+print("out from loop")
+
+# Output:
+1
+2
+3
+4
+5
+6
+out from loop
