23b1ec8fc354c9e355742669095cacb29320b232
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-29 13:36:36 +0530

    while_loop(3).py

---
+++ Loops/while_loop(3).py
@@ -0,0 +1,16 @@
+number=int(input("enter a number(-1 to quit)"))
+while number != -1:
+    print(number)
+    number= number=int(input("enter a number(-1 to quit)"))
+else:
+    print("in else block")
+print("Out of loop")
+
+# Output : 
+enter a number(-1 to quit)8
+8
+enter a number(-1 to quit)-9
+-9
+enter a number(-1 to quit)-1
+in else block
+Out of loop
