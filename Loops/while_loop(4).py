a2d5be7e2045990b51351caf3c7514e9c0c1499c
Vivek Singh <137812531+vivek2431@users.noreply.github.com>
2024-04-29 13:40:39 +0530

    while_loop(4).py

---
+++ Loops/while_loop(4).py
@@ -0,0 +1,13 @@
+total=0
+number=int(input("enter a number(0 to quit)"))
+while number!=0:
+    total = total+number
+    number=int(input("enter a number(0 to quit)"))
+print("total is", total)
+
+# Output :
+enter a number(0 to quit)5
+enter a number(0 to quit)4
+enter a number(0 to quit)-4
+enter a number(0 to quit)0
+total is 5
